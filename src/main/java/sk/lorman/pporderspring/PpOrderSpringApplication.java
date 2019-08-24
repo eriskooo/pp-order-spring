d164d4a10fe5b2c45c34f38499cadd1f82c305a2
eriskooo <lorman@pobox.sk>
2019-08-24 15:22:33 +0200

    Initial commit

---
+++ src/main/java/sk/lorman/pporderspring/PpOrderSpringApplication.java
@@ -0,0 +1,13 @@
+package sk.lorman.pporderspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpOrderSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpOrderSpringApplication.class, args);
+    }
+
+}
