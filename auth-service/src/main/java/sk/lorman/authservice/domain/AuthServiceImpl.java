0b7478a8d607008f2af680c75f20a5ae40b7ea7d
eriskooo <lorman@pobox.sk>
2019-08-28 12:30:15 +0200

    init

--- auth-service/src/main/java/sk/lorman/authservice/domain/AuthServiceImpl.java
+++ auth-service/src/main/java/sk/lorman/authservice/domain/AuthServiceImpl.java
@@ -8,8 +8,6 @@ import sk.lorman.authservice.infrastructure.exception.AuthException;
 import sk.lorman.authservice.infrastructure.properties.ConfigProperties;
 import sk.lorman.authservice.infrastructure.security.SecurityConstants;
 
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Service
@@ -28,38 +26,15 @@ public class AuthServiceImpl implements AuthService {
     }
 
     private String getTokenForUser(String user) {
-
-        ZonedDateTime now = ZonedDateTime.now();
-        Date issuedAt = toDate(now);
-        Date expiresAt = toDate(now.plus(15, ChronoUnit.MINUTES));
-        Date notBefore = toDate(now);
-
         return Jwts.builder()
                 .signWith(Keys.hmacShaKeyFor(properties.getSigningKey().getBytes()), SignatureAlgorithm.HS512)
                 .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
                 .setIssuer(SecurityConstants.TOKEN_ISSUER)
                 .setAudience(SecurityConstants.TOKEN_AUDIENCE)
-
                 .setSubject(user)
                 .setExpiration(new Date(System.currentTimeMillis() + 864000000))
-                .claim("rol", "user")
+                .claim("role", "user")
                 .compact();
-
-
-//        return JWT.create()
-//                .withIssuer(issuer)
-//                .withIssuedAt(issuedAt)
-//                .withExpiresAt(expiresAt)
-//                .withNotBefore(notBefore)
-//                .withSubject(credentialDTO.getUsername())
-//                .withClaim(OPEN_ID_STANDARD_CLAIM_FIRSTNAME, credentialDTO.getUsername())
-//                .withClaim(OPEN_ID_STANDARD_CLAIM_LASTNAME, credentialDTO.getUsername())
-//                .withClaim(CUSTOM_CLAIM_ROLES, "admin")
-//                .sign(Algorithm.HMAC256(secretKey));
-    }
-
-    private Date toDate(final ZonedDateTime zonedDateTime) {
-        return Date.from(zonedDateTime.toInstant());
     }
 
     private void validate(String user, String pwd) {
